dc7de94ca86512d23104791b4b6268d94d4494a6
LloydTea <Lloyd.mft@gmail.com>
2022-09-19 21:25:12 +0400

    Code Update

--- Promo Code For CF.js
+++ Promo Code For CF.js
@@ -1,5 +1,5 @@
-var mainProd = '4257649';
-var promoProd = '4257652';
+var mainProd = 'mainProd';
+var promoProd = 'promoProd';
 var promoCodes = ['PROMOCODE']; //this should always be uppercase
 var head = '<div class="de elHeadlineWrapper de-editable" id="headline-72651" data-de-type="headline" data-de-editing="false" data-title="headline" data-ce="true" data-trigger="none" data-animate="fade" data-delay="500" style="margin-top: 20px; cursor: pointer; outline: none; display: block;"><div class="ne elHeadline lh3 elMargin0 elBGStyle0 hsTextShadow0 elFont_raleway hsSize27" style="text-align: left;" data-bold="inherit" contenteditable="false"><b>Have a Promo Code?</b></div></div>';
 var field = '<input type=\'text\' id=\'promo_code\' name=\'promo_code\' placeholder=\'Enter promo code...\' class=\'elInput elInput100 elAlign_left elInputSmall elInputStyl0 elInputBG1 elInputBR5 elInputI0 elInputIBlack elInputIRight elInputStyle1 elInputSmall garlic-auto-save\' />';
